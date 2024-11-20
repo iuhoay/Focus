49a4219d2e30a05a1568236eba9b34e86d7a7628
iuhoay <iuhoay@gmail.com>
2024-11-20 22:21:41 +0800

    Initial Commit

---
+++ Focus/FocusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FocusApp.swift
+//  Focus
+//
+//  Created by Jack Wu on 11/20/24.
+//
+
+import SwiftUI
+
+@main
+struct FocusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
